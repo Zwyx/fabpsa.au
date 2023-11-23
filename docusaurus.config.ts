bf1d7a30f50e83e6a1cd4375c91d5922b6f3da91
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-11-23 19:44:24 +0800

    Upgrade Docusaurus
    Same as done in github.com/zwyx/zwyx.dev

---
+++ docusaurus.config.ts
@@ -0,0 +1,126 @@
+import type { Options, ThemeConfig } from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import dotenv from "dotenv";
+import { themes } from "prism-react-renderer";
+import { env, exit } from "process";
+
+dotenv.config();
+
+const theme = themes.github;
+const darkTheme = themes.dracula;
+
+const DOMAIN = env.DOMAIN;
+const PLAUSIBLE_SCRIPT_SRC = env.PLAUSIBLE_SCRIPT_SRC;
+
+[DOMAIN, PLAUSIBLE_SCRIPT_SRC].forEach((value) => {
+	if (!value) {
+		console.error(`Missing environment variable`);
+		exit(1);
+	}
+});
+
+const config: Config = {
+	title: "FABPSA",
+	tagline: "French Australian Bilingual Program for School Association",
+	favicon: "img/favicon.png",
+	url: `https://${DOMAIN}`,
+	baseUrl: "/",
+
+	trailingSlash: false,
+
+	scripts: [
+		{
+			src: PLAUSIBLE_SCRIPT_SRC,
+			defer: true,
+			"data-domain": DOMAIN,
+		},
+		{
+			src: "https://f.convertkit.com/ckjs/ck.5.js",
+		},
+	],
+
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
+
+	markdown: {
+		mdx1Compat: {
+			admonitions: false,
+			comments: false,
+			headingIds: false,
+		},
+	},
+
+	presets: [
+		[
+			"classic",
+			{
+				docs: false,
+				blog: false,
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			} satisfies Options,
+		],
+	],
+
+	themeConfig: {
+		metadata: [
+			{
+				name: "keywords",
+				content: "french, english, bilingual Program, school",
+			},
+		],
+		colorMode: {
+			respectPrefersColorScheme: true,
+			disableSwitch: true,
+		},
+		image: "img/fabpsa.webp",
+		navbar: {
+			logo: {
+				alt: "Fabpsa logo",
+				src: "img/fabpsaSmall.webp",
+				srcDark: "img/fabpsaDarkSmall.webp",
+				href: "/",
+			},
+			title: "FABPSA",
+			items: [
+				{ to: "/about-us", label: "About us" },
+				{
+					type: "dropdown",
+					label: "Our project",
+					items: [
+						{ to: "/our-vision", label: "Our vision" },
+						{ to: "/our-goals", label: "Our goals" },
+						{ to: "/our-action-plan", label: "Our action plan" },
+					],
+				},
+				{ to: "/the-program", label: "The program" },
+				{ to: "/our-team", label: "Our team" },
+				{ to: "/supporting-us", label: "Supporting us" },
+				{ to: "/resources", label: "Resources" },
+				{ to: "mailto:contact@fabpsa.au", label: "Contact us" },
+			],
+		},
+		footer: {
+			style: "dark",
+			copyright: `Copyright © ${new Date().getFullYear()} FABPSA Inc/ACNC`,
+			logo: {
+				alt: "Fabpsa logo",
+				src: "img/fabpsaDarkSmall.webp",
+				href: "/",
+				width: "75px",
+				height: "75px",
+				className: "footerImage",
+			},
+		},
+		prism: {
+			theme,
+			darkTheme,
+			additionalLanguages: ["bash", "json"],
+		},
+	} satisfies ThemeConfig,
+};
+
+export default config;
